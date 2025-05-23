36b33b8adafeef9cef489c3d695119300a06d1f6
Johnell <empuertojohnellchess@gmail.com>
2025-05-23 08:42:21 +0800

    Initial Node-RED setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const http = require("http");
+const express = require("express");
+const RED = require("node-red");
+
+const app = express();
+const server = http.createServer(app);
+
+const settings = {
+  httpAdminRoot: "/",
+  httpNodeRoot: "/",
+  userDir: "./.nodered/",
+  functionGlobalContext: {},
+};
+
+RED.init(server, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(process.env.PORT || 1880);
+RED.start();
